91f6109434d69fc3f959153ab3c6673dabd15a15
Arsenii Liunsha <me@arslee.dev>
2021-12-07 14:21:56 +0900

    chore: Edit example

--- example/example.dart
+++ example/example.dart
@@ -2,8 +2,20 @@ import 'package:anilibria/anilibria.dart';
 
 void main() async {
   final anilibria = Anilibria(Uri.parse('https://api.anilibria.tv/v2'));
-  final titles = await anilibria.getUpdates(limit: 10);
+
+  // Fetch one title
+  final title = await anilibria.getTitle(id: 824);
+  print(title.names?.en);
+
+  // Fetch multiple titles
+  final titles = await anilibria.getTitles(idList: [3542, 9006]);
   for (final title in titles) {
     print(title.names?.en);
+  }
+
+  // Fetch updated titles
+  final updates = await anilibria.getUpdates(limit: 10);
+  for (final update in updates) {
+    print(update.names?.en);
   }
 }
