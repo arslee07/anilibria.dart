d39ec9cf37b6991b1f229f588e92f81063ea192e
arslee <me@arslee.dev>
2022-03-24 21:48:15 +0900

    chore: Export enums based on status code

--- lib/anilibria.dart
+++ lib/anilibria.dart
@@ -3,6 +3,9 @@ export 'src/anilibria.dart';
 export 'src/enums/description_type.dart';
 export 'src/enums/playlilst_type.dart';
 export 'src/enums/include.dart';
+export 'src/enums/codes/season_code.dart';
+export 'src/enums/codes/title_status_code.dart';
+export 'src/enums/codes/title_type_code.dart';
 
 export 'src/models/title.dart';
 export 'src/models/youtube.dart';
