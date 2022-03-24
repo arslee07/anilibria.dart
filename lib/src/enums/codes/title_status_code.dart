619f2e7ce427ed59909da3aec7975ec1858bbcd6
arslee <me@arslee.dev>
2022-03-24 21:59:19 +0900

    fix(TitleStatusCode): Fix values

--- lib/src/enums/codes/title_status_code.dart
+++ lib/src/enums/codes/title_status_code.dart
@@ -1,10 +1,10 @@
 import 'package:anilibria/src/utils/enum.dart';
 
 class TitleStatusCode extends IEnum<int> {
-  static const inWork = TitleStatusCode(0);
-  static const completed = TitleStatusCode(1);
-  static const hidden = TitleStatusCode(2);
-  static const notOngoing = TitleStatusCode(3);
+  static const inWork = TitleStatusCode(1);
+  static const completed = TitleStatusCode(2);
+  static const hidden = TitleStatusCode(3);
+  static const notOngoing = TitleStatusCode(4);
 
   TitleStatusCode.fromValue(int value) : super(value);
   const TitleStatusCode(int value) : super(value);
